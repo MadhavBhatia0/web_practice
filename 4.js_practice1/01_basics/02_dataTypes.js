2232eb7b04e4ac74dd4797ba3a2e84866d6ac85d
Madhav <madhavbhatia150@gmail.com>
2024-08-03 18:14:36 +0530

    add 02_dataTypes

---
+++ 4.js_practice1/01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+// console.log(3+3) console.log("Maddy")// will not work
+// console.log(3+3); console.log("Maddy") //now work
+
+let name = "maddy"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value //an object //representation of empty
+// undefined =>             // not assigned
+// symbol => unique
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof age);
