3431d5a1a7347d3db8694ef807317647c5002205
Madhav <madhavbhatia150@gmail.com>
2024-08-03 18:05:22 +0530

    add 01_variables

---
+++ 4.js_practice1/01_basics/01_variables.js
@@ -0,0 +1,20 @@
+console.log("Hello world!");
+
+const accountId = 144553
+let accountEmail = "maddy@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // cannot change constant
+
+// accountCity="bathinda"
+// console.log(accountCity) //possible
+
+// const H
+// H="hello" //cant leave uninitialised
+
+// var accountPassword="555"
+// console.log(accountPassword) //possible
+
+// let accountEmail = "google.com" // not possible
