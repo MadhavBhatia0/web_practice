b626cc989dea4136ff8df73fcd30d714fa15b248
Madhav <madhavbhatia150@gmail.com>
2024-10-09 22:22:40 +0530

    static

---
+++ 11.classes_oop/7_stactic.js
@@ -0,0 +1,26 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){ // static doesnot give access
+        return `123`   // used when we don't want to give access to information
+    }
+}
+
+const hitesh = new User("hitesh")
+// console.log(hitesh.createId())
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.createId());
